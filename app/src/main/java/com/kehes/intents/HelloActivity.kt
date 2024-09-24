4630a406616cba7d684a6726d8b64591f71263e1
Vampirish <vamperish@gmail.com>
2024-09-25 01:27:45 +0200

    app can take text type from other activities

--- app/src/main/java/com/kehes/intents/HelloActivity.kt
+++ app/src/main/java/com/kehes/intents/HelloActivity.kt
@@ -1,5 +1,6 @@
 package com.kehes.intents
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.kehes.intents.databinding.ActivityHelloBinding
@@ -14,7 +15,11 @@ class HelloActivity : AppCompatActivity() {
 
         intent.extras?.let {
             val name = it.getString(ArgumentKey.NAME.name)
-            binding.helloView.text = getString(R.string.hello_any, name)
+            val newName = it.getString(Intent.EXTRA_TEXT)
+
+            var showName: String? = null
+            showName = newName ?: name
+            binding.helloView.text = getString(R.string.hello_any, showName)
         }
     }
 
